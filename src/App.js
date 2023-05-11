168eb93b4a68562a852c1cea1ae83bf6acff6663
github-aliceyang <aliyang47@gmail.com>
2023-05-11 15:23:59 -0700

    updated weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react'
 import './App.css';
-
+import Weather from './components/Weather'
 
 function App() {
   return (
     <div className="App">
-     hello react weather
+     <Weather />
     </div>
   );
 }
