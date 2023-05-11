5c3e94c797040b0cdba4bb439468ce8180b9a9fc
github-aliceyang <aliyang47@gmail.com>
2023-05-11 15:24:19 -0700

    created weather info file

---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WeatherInfo(props) {
+    return (
+        <div className="WeatherInfo">
+           
+        </div>
+    );
+}
+
+export default WeatherInfo
