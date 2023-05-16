28046698daef56dc548300f6b203ea12c14e6191
github-aliceyang <aliyang47@gmail.com>
2023-05-15 17:02:40 -0700

    created temperature component

---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import "../stylesheets/WeatherTemperature.css";
+
+function WeatherTemperature(props) {
+    const [unit, setUnit] = useState('celsius')
+
+    const displayCelsius = (e) => {
+        e.preventDefault();
+        setUnit("celsius")
+    }
+
+    const displayFahrenheit = (e) => {
+        e.preventDefault();
+        setUnit("fahrenheit")
+    }
+
+    const fahrenheit = (e) => {
+        return (props.celsius * 9) / 5 + 32;
+    }
+
+    if (unit === "celsius") {
+        return (
+            <div className='temp-conversion'>
+                <span className='temp'>
+                    {Math.round(props.celsius)} 
+                    </span>
+                <span className='unit'>{" "} C°
+                |
+                <a href="/" onClick={displayFahrenheit}>F°</a>
+                </span>
+            </div>
+
+        )
+    } else {
+        return (
+            <div className='temp-conversion'>
+                <span className='temp'>
+                    {Math.round(fahrenheit())}</span>
+                <span className='unit'>
+                    <a href="/" onClick={displayCelsius}>C°</a>
+                    |{" "}
+                     F°
+                </span>
+            </div>
+
+        )
+
+    }
+
+
+}
+
+export default WeatherTemperature
