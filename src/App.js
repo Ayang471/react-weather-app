b3802969da711a7e52047ae83ae1bb3cd16b07d0
github-aliceyang <aliyang47@gmail.com>
2023-05-15 17:04:06 -0700

    updated app js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Weather from './components/Weather'
 function App() {
   return (
     <div className="App">
-     <Weather />
+      <div className="container">
+        <Weather defaultCity="San Jose" />
+      </div>
+      <footer> This app was coded by <span className="name"> Alice Yang </span>and is <a href="https://github.com/Ayang471/react-project/tree/main" target="_blank" rel="noreferrer">open-sourced on GitHub </a>
+        and hosted on <a href="https://glowing-bonbon-bc9d7c.netlify.app/" target="_blank" rel="noreferrer"> Netlify.</a></footer>
     </div>
   );
 }
